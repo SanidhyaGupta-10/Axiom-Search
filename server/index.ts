91bdbd8dabaf57a4d40a9c8252f32e8980999bcc
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-07-31 20:25:00 +0530

    feat: initialize server project with prompt templates and express dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,25 @@
 import express from 'express';
+import { tavily } from '@tavily/core';
 
 const app = express();
+const client = tavily({ apiKey: process.env.TAVILY_API_KEY })
 
-app.post('/conversation', () => {
+app.use(express.json());
+
+app.post('/perplexity-ask', async (req, res) => {
     // get the query from the user
+    const query = req.body.query;
 
     // make sure that user has access/credits to hit the endpoint
 
     // check it we have web search for a similar query
 
-    // web search to gather sources
+    // web search to gather sources ( Step-4)
+    const webSearchResponse = await client.search(query, {
+        searchDepth: "advanced"
+    })
+
+    const webSearchResults = webSearchResponse.results;
 
     // do some context engineering on the prompt + web search responses
 
