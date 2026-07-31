3029c2bec9c77762e31ebe820c82ac0aae15d85a
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-07-31 20:01:03 +0530

    Flowchart of the project

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,19 @@ import express from 'express';
 const app = express();
 
 app.post('/conversation', () => {
+    // get the query from the user
 
+    // make sure that user has access/credits to hit the endpoint
+
+    // check it we have web search for a similar query
+
+    // web search to gather sources
+
+    // do some context engineering on the prompt + web search responses
+
+    // hit the LLM and stream back the response
+
+    // also follow up response - (streaming)
 });
 
 app.listen(3000);
